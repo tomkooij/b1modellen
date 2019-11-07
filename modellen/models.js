d2aa9a6ca583eced48b27bf78f2e0239d9aee515
Tom Kooij <tomkooij@tomkooij.nl>
2019-11-07 16:12:01 +0100

    Add gedempte trilling

--- modellen/models.js
+++ modellen/models.js
@@ -2,4 +2,5 @@
 var model_index = [
 		{title: "0 Leeg model", url: "modellen/0_Leeg_model.xml"},
 		{title: "1 Harmonische trilling", url: "modellen/1_Harmonische_trilling.xml"},
+		{title: "2 Gedempte trilling", url: "modellen/2_Gedempte_trilling.xml"},
 ];
